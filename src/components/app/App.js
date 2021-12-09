a6bdbb0702864d9e11a0e56886cbde5cc1e69ca4
Andrii Pashulia <pashulyaand@gmail.com>
2021-12-09 16:49:17 +0200

    created HomePage Component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -8,6 +8,7 @@ import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import decoration from "../../resources/img/vision.png";
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner";
+import HomePage from "../pages/HomePage";
 
 const App = () => {
   const [selectedChar, setSelectedChar] = useState(null);
@@ -21,28 +22,7 @@ const App = () => {
       <AppHeader />
       <main>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <ErrorBoundary>
-                  <RandomChar />
-                </ErrorBoundary>
-                <div className="char__content">
-                  <ErrorBoundary>
-                    <CharList
-                      onCharSelected={onCharSelected}
-                      selectedChar={selectedChar}
-                    />
-                  </ErrorBoundary>
-                  <ErrorBoundary>
-                    <CharInfo selectedChar={selectedChar} />
-                  </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" />
-              </>
-            }
-          />
+          <Route path="/" element={<HomePage></HomePage>} />
           <Route
             path="/comics"
             element={
